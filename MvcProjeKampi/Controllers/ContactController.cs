599d9a5218630417874c6bcbee459acd83a8dd61
mehmetyildizz <yildizzmehmet@yandex.com>
2021-06-08 23:42:42 +0300

    66. Ders Sonu

--- MvcProjeKampi/Controllers/ContactController.cs
+++ MvcProjeKampi/Controllers/ContactController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,7 @@ namespace MvcProjeKampi.Controllers
     {
         ContactManager cm = new ContactManager(new EfContactDal());
         ContactValidator contactValidator = new ContactValidator();
+        Context context = new Context();
 
         public ActionResult Index()
         {
@@ -28,6 +30,21 @@ namespace MvcProjeKampi.Controllers
 
         public PartialViewResult MesajKlasorPartial()
         {
+            var IletisimMesajSayiyisi = context.Contacts.Count().ToString();
+            ViewBag.mesajsayisiIletisim = IletisimMesajSayiyisi;
+
+            var GelenMesajSayisi = context.Messages.Count(x => x.MessageReciever == "admin@yandex.com" && x.MessageStatusReceiver == false).ToString();
+            ViewBag.mesajsayisiGelen = GelenMesajSayisi;
+
+            var GidenMesajSayisi = context.Messages.Count(x => x.MessageSender == "admin@yandex.com" && x.MessageStatusSender == false).ToString();
+            ViewBag.mesajsayisiGiden = GidenMesajSayisi;
+
+            var TaslakMesajSayisi = context.Messages.Count(x => x.MessageStatusDraft == true).ToString();
+            ViewBag.mesajsayisiTaslak = TaslakMesajSayisi;
+
+            var SilinenMesajSayisi = context.Messages.Count(x => x.MessageStatusReceiver == true || x.MessageStatusSender == true).ToString();
+            ViewBag.mesajsayisiSilinen = SilinenMesajSayisi;
+
             return PartialView();
         }
     }
--- MvcProjeKampi/Controllers/MessageController.cs
+++ MvcProjeKampi/Controllers/MessageController.cs
@@ -6,6 +6,7 @@ using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 
@@ -21,7 +22,7 @@ namespace MvcProjeKampi.Controllers
             var mesajlistesi = mm.MesajListeGetirGelen();
             return View(mesajlistesi);
         }
-        
+
         public ActionResult GidenKutusu()
         {
             var mesajlistesi = mm.MesajListeGetirGiden();
@@ -46,7 +47,9 @@ namespace MvcProjeKampi.Controllers
             return View();
         }
 
+
         [HttpPost, ValidateInput(false)]
+        [MultipleButton(Name = "mesaj", Argument = "Gonder")]
         public ActionResult YeniMesajGonder(Message p)
         {
             ValidationResult results = messageValidator.Validate(p);
@@ -54,6 +57,7 @@ namespace MvcProjeKampi.Controllers
             {
                 p.MessageSender = "admin@yandex.com";
                 p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+
                 mm.MesajEkle(p);
                 return RedirectToAction("GidenKutusu");
             }
@@ -66,6 +70,38 @@ namespace MvcProjeKampi.Controllers
             }
             return View();
         }
+
+        [HttpPost, ValidateInput(false)]
+        [MultipleButton(Name = "mesaj", Argument = "Taslak")]
+        public ActionResult Taslak(Message p)
+        {
+            p.MessageSender = "admin@yandex.com";
+            p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.MessageStatusDraft = true;
+            mm.MesajEkle(p);
+            return RedirectToAction("TaslakMesajlar");
+        }
+
+
+        //public ActionResult YeniMesajGonder(Message p)
+        //{
+        //    ValidationResult results = messageValidator.Validate(p);
+        //    if (results.IsValid)
+        //    {
+        //        p.MessageSender = "admin@yandex.com";
+        //        p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+        //        mm.MesajEkle(p);
+        //        return RedirectToAction("GidenKutusu");
+        //    }
+        //    else
+        //    {
+        //        foreach (var item in results.Errors)
+        //        {
+        //            ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+        //        }
+        //    }
+        //    return View();
+        //}
 
         public ActionResult GelenMesajDetay(int id)
         {
@@ -93,6 +129,28 @@ namespace MvcProjeKampi.Controllers
             mesajdeger.MessageStatusReceiver = true;
             mm.MesajSil(mesajdeger);
             return RedirectToAction("GelenKutusu");
+        }
+
+        [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+        public class MultipleButtonAttribute : ActionNameSelectorAttribute
+        {
+            public string Name { get; set; }
+            public string Argument { get; set; }
+
+            public override bool IsValidName(ControllerContext controllerContext, string actionName, MethodInfo methodInfo)
+            {
+                var isValidName = false;
+                var keyValue = string.Format("{0}:{1}", Name, Argument);
+                var value = controllerContext.Controller.ValueProvider.GetValue(keyValue);
+
+                if (value != null)
+                {
+                    controllerContext.Controller.ControllerContext.RouteData.Values[Name] = Argument;
+                    isValidName = true;
+                }
+
+                return isValidName;
+            }
         }
     }
 }
