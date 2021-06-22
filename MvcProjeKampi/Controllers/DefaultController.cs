58fdb0f81df214cbe8eec1845d542e4f15847ed5
mehmetyildizz <yildizzmehmet@yandex.com>
2021-06-23 00:18:26 +0300

    81. Ders Sonu

---
+++ MvcProjeKampi/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    [AllowAnonymous]
+    public class DefaultController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+
+        public ActionResult Index()
+        {
+            var baslikListe = hm.BaslikListeGetir();
+            return View(baslikListe);
+        }
+
+        public ActionResult Basliklar()
+        {
+            return View();
+        }
+    }
+}
