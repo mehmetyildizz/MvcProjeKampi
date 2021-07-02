659beb3fbf6628b90a3e7753a4c1b97dc77820b7
mehmetyildizz <mehmetyildizz@hotmail.com>
2021-07-02 16:22:44 +0300

    94. Ders Sonu

---
+++ MvcProjeKampi/Models/CategoryClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcProjeKampi.Models
+{
+    public class CategoryClass
+    {
+        public string KategoriAdi { get; set; }
+        public int KategoriSayisi { get; set; }
+    }
+}
